e345e091aa127af45f9795f887d454818b3ea5e0
旺仔 <weiwang1202@126.com>
2016-03-09 17:14:33 +0800

    1.定时发送

--- SendMsg/ViewController.m
+++ SendMsg/ViewController.m
@@ -9,7 +9,8 @@
 #import "ViewController.h"
 
 @interface ViewController ()
-
+@property (weak, nonatomic) IBOutlet UIButton *sendButton;
+@property (nonatomic, strong) NSTimer *currentTimer;
 @end
 
 @implementation ViewController
@@ -17,6 +18,28 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (IBAction)sendClick {
+    [_sendButton setImage:[UIImage imageNamed:@"timerBtn_imageView_selected"] forState:UIControlStateNormal];
+    _currentTimer = [NSTimer scheduledTimerWithTimeInterval:1.0 target:self selector:@selector(timeShow) userInfo:nil repeats:YES];
+    [_currentTimer fire];
+}
+
+- (void)timeShow {
+    static int index = 10;
+    index--;
+    if (index==0) {
+        NSLog(@"结束了。");
+        [_sendButton setTitle:@"重新发送" forState:UIControlStateNormal];
+        [_sendButton setImage:[UIImage imageNamed:@"timerBtn_imageView_normal"] forState:UIControlStateNormal];
+        [_currentTimer invalidate];
+        index = 10;
+        self.view.userInteractionEnabled = YES;
+        return;
+    }
+    self.view.userInteractionEnabled = NO;
+    [self.sendButton setTitle:[NSString stringWithFormat:@"还剩%d秒", index] forState:UIControlStateNormal];
 }
 
 - (void)didReceiveMemoryWarning {
